fea2b9a68e420224087f6fe9419f17a7f6fad438
Khanh Vuong <khanhvuong987@gmail.com>
2020-12-09 21:17:18 +0700

    KitchenDemo

---
+++ KitchenProjectDemo/app.js
@@ -0,0 +1,91 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+
+
+//Slider(Banner)
+
+const IMG_WIDTH = 1540;
+const slideContainer = $(".slides");
+const slideImage = [...$$(".slide")];
+
+//Button
+const nextBtn = $("#next");
+const preBtn = $("#pre");
+
+slideContainer.style.width = slideImage.length * IMG_WIDTH + "px";
+
+let counter = 0;
+
+function next() {
+  if (counter < slideImage.length - 1) {
+    counter++;
+  } else {
+    counter = 0;
+  }
+  slideContainer.style.transition = "transform 0.4s ease-in-out";
+  slideContainer.style.transform = "translateX(-" + IMG_WIDTH * counter + "px)";
+}
+
+
+setInterval(() => {
+    next();
+  }, 5000);
+
+nextBtn.addEventListener('click',()=>{
+    if(counter < slideImage.length - 1){
+      counter++;
+    }else{
+      counter=0;
+    }
+    slideContainer.style.transition = "transform 0.4s ease-in-out";
+    slideContainer.style.transform = 'translateX('+ (-IMG_WIDTH * counter) + 'px)';
+});
+
+preBtn.addEventListener('click',()=>{
+    if(counter == 0){
+      counter = slideImage.length - 1;
+    }
+    else{
+      counter --;
+    }
+    slideContainer.style.transition = "transform 0.4s ease-in-out";
+    slideContainer.style.transform = 'translateX('+ (-IMG_WIDTH * counter) + 'px)';
+});
+
+
+
+//Product Demo
+
+const productList = $('.product-list');
+const prouductItem = [...$$('.product')];
+
+const nextPro = $('#nextPro');
+const prePro = $('#prePro');
+
+const imgProduct = 270;
+
+productList.style.width = prouductItem.length * imgProduct + "px";
+
+let index = 0;
+
+nextPro.addEventListener('click',()=>{
+    if(index < slideImage.length - 1){
+      index++;
+    }else{
+      index=0;
+    }
+    productList.style.transition = "transform 0.4s ease-in-out";
+    productList.style.transform = 'translateX('+ (- imgProduct * index) + 'px)';
+});
+
+prePro.addEventListener('click',()=>{
+    if(index == 0){
+      index = slideImage.length - 1;
+    }
+    else{
+      index --;
+    }
+    productList.style.transition = "transform 0.4s ease-in-out";
+    productList.style.transform = 'translateX('+ (-imgProduct * index) + 'px)';
+});
