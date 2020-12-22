6caef01d76c5fedec150e0881cd24c2ab3a89c12
Khanahs <khanhvuong247@gmail.com>
2020-12-22 12:04:33 +0700

    product category

--- Admin/js/app.js
+++ Admin/js/app.js
@@ -1,11 +1,11 @@
-const layouts = document.querySelectorAll('.layouts > li');
+// const layouts = document.querySelectorAll('.layouts > li');
+// const childLayout = document.querySelector('.child-layout');
 
-
-layouts.forEach(item =>{
-    item.addEventListener('click',function(){
-        layouts.forEach(item =>{
-            item.classList.remove('active-menu');
-        })
-        item.classList.add('active-menu');
-    });
-});
+// layouts.forEach(item => {
+//     item.addEventListener('click', function () {
+//         layouts.forEach(item => {
+//             item.classList.remove('active-menu');
+//         });
+//         item.classList.add('active-menu');
+//     });
+// });
